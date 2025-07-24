e55de896fba247ccc0d619230179936197cf9bbb
NicolasFukunaga <fukunaganicolas@outlook.com>
2025-07-24 14:49:17 -0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,8 @@
+let nomeLista = true
+let temIngresso = false 
+
+if nomeLista || temIngresso {
+    print("Pode entrar na festa.")
+} else {
+    print("Não pode entrar na festa.")
+}
---
+++ main2.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+print("Informe a velocidade:")
+
+let velocidade = Int(readLine()!)!
+
+
+if velocidade > 60 {
+    print("Multado!")
+} else {
+    print("Não multado!")
+}
