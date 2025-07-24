3c5c1c5f2b09f6e1a416c95151e82beb1beb7cc8
NicolasFukunaga <fukunaganicolas@outlook.com>
2025-07-24 15:09:15 -0300

    Add files via upload

---
+++ main3.swift
@@ -0,0 +1,15 @@
+import Foundation 
+
+print("Informe a cor do farol: ")
+
+let farol = "amarelo"
+
+if farol == "amarelo" {
+    print("Preste atenção!")
+} else if farol == "verde" {
+    print("Prossiga!")
+} else if farol == "vermelho" {
+    print("Fechado")
+} else {
+    print("Cor inválida")
+}
